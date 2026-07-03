7bc12fe0b261d6246cd36901f4fb3b618456c398
pakho2433 <pakho2433@gmail.com>
2026-07-04 00:17:44 +0800

    rename app to school reading programme title

--- app-config.js
+++ app-config.js
@@ -8,7 +8,7 @@ const CLASSROOM_NAMES = [
 ];
 
 export const APP_CONFIG = {
-  schoolName: "全校閱讀跑道",
+  schoolName: "東華三院周演森小學-26/27悅讀千里號",
   maxRunnersPerClass: 26,
   trackDistance: 15000,
   stageDistance: 1500,
