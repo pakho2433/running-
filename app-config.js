ebf6a332530bfb2eb8229323dab0d643ff03214f
pakho2433 <pakho2433@gmail.com>
2026-07-03 23:43:33 +0800

    rename classrooms to 1A through 6C

--- app-config.js
+++ app-config.js
@@ -1,3 +1,12 @@
+const CLASSROOM_NAMES = [
+  "1A", "1B",
+  "2A", "2B", "2C",
+  "3A", "3B", "3C",
+  "4A", "4B", "4C",
+  "5A", "5B", "5C",
+  "6A", "6B", "6C",
+];
+
 export const APP_CONFIG = {
   schoolName: "全校閱讀跑道",
   maxRunnersPerClass: 26,
@@ -11,8 +20,8 @@ export const APP_CONFIG = {
     subject: 30,
     completion: 50,
   },
-  classrooms: Array.from({ length: 17 }, (_, index) => ({
+  classrooms: CLASSROOM_NAMES.map((name, index) => ({
     id: `C${String(index + 1).padStart(2, "0")}`,
-    name: `課室 ${String(index + 1).padStart(2, "0")}`,
+    name,
   })),
 };
