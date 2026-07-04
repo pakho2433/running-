101f61d86d6ca010857a727651df2bcc5b8f4e29
pakho2433 <pakho2433@gmail.com>
2026-07-04 10:36:54 +0800

    disable raw Firebase access in student app

---
+++ firebase-student-disabled.js
@@ -0,0 +1,9 @@
+// Emergency privacy mode: student-facing modules must not connect to raw Firestore data.
+export const firebaseConfig = {
+  apiKey: "DISABLED",
+  authDomain: "DISABLED",
+  projectId: "DISABLED",
+  storageBucket: "DISABLED",
+  messagingSenderId: "DISABLED",
+  appId: "DISABLED",
+};
