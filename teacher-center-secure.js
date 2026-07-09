5abe15d1a72c5c1f9e4b307b5e6559efbfc94bc2
pakho2433 <pakho2433@gmail.com>
2026-07-09 19:12:41 +0800

    Delete disabled teacher center shim

--- teacher-center-secure.js
+++
@@ -1,3 +0,0 @@
-// Teacher centre is temporarily disabled by request.
-// This no-op module prevents teacher login buttons and dialogs from being added while keeping imports safe.
-window.READING_RUN_TEACHER_CENTER_DISABLED = true;
