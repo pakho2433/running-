238a3157f3381ac7f6bdb8406316e39e20ad2866
pakho2433 <pakho2433@gmail.com>
2026-08-19 23:44:57 +0800

    Bump staging UI cache for reading-date policy

--- app-stage.js
+++ app-stage.js
@@ -1,2 +1,2 @@
-import "./secure-password-ui-app.js?v=20260709-teacher-1";
+import "./secure-password-ui-app.js?v=20260819-reading-date-only-1";
 import "./reading-buddy-bootstrap.js?v=20260819-reading-date-fix-2";
