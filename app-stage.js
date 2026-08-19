6c36be6b4298cadcf4bcff07f8c53d49a1ef30af
pakho2433 <pakho2433@gmail.com>
2026-08-20 00:05:33 +0800

    Load student-name UI in staging

--- app-stage.js
+++ app-stage.js
@@ -1,2 +1,3 @@
 import "./secure-password-ui-app.js?v=20260819-reading-date-only-1";
 import "./reading-buddy-bootstrap.js?v=20260819-reading-date-fix-2";
+import "./student-name-ui.js?v=20260820-staging-names-1";
